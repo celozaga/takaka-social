0ea44ba918482ee233667ded5c4c70e42a8219f2
Celo Zaga <69090827+celozaga@users.noreply.github.com>
2025-08-20 01:47:11 -0300

    fix: Remove unnecessary trailing newlines
    Removes trailing newlines from PostCard.tsx and PostScreen.tsx to improve code cleanliness.

--- components/post/PostCard.tsx
+++ components/post/PostCard.tsx
@@ -1,6 +1,3 @@
-
-
-
 import React from 'react';
 import { AppBskyFeedDefs, AppBskyEmbedImages,AppBskyActorDefs, RichText, AppBskyEmbedRecord, AppBskyEmbedRecordWithMedia, AppBskyEmbedVideo } from '@atproto/api';
 import { useAtp } from '../../context/AtpContext';
--- components/post/PostScreen.tsx
+++ components/post/PostScreen.tsx
@@ -1,5 +1,3 @@
-
-
 import React, { useState, useEffect, useRef, lazy, Suspense } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useAtp } from '../../context/AtpContext';
